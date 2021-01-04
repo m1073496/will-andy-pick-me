51ec2d2d0964e2ba6133b060f9c260bc3213093a
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-01-04 11:41:36 -0700

    Add Q/A html element and corresponding styling

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var userQuestion = document.querySelector(".question-field");
 var submitButton = document.querySelector("#submit-button");
 var eightBall = document.querySelector(".eight-ball-img");
+var answerDisplay = document.querySelector(".answer-box");
 
 
 submitButton.addEventListener("click", showAnswer);
 
 function showAnswer() {
   eightBall.classList.add("hidden");
+  answerDisplay.innerHTML =
+    `<section class="q-a-box">
+     <h4 class="user-question">""</h4>
+     <h3 class="random-answer"></h3>
+     </section>`;
 };
